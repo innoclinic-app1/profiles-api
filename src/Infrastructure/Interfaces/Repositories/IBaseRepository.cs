431aa32c969fd98be105f900334a83ad863c00f3
Andrew Shumel <155365407+capybo@users.noreply.github.com>
2024-01-12 14:53:38 +0300

    Added interfaces for repositories

---
+++ src/Infrastructure/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Interfaces.Repositories;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task InsertAsync(T entity);
+    
+    Task RemoveAsync(T entity);
+    
+    Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    
+    Task<IQueryable<T>> GetManyAsync(int skip, int take, CancellationToken cancellationToken = default);
+    
+    Task<IQueryable<T>> GetByNameAsync(string name, CancellationToken cancellationToken = default);
+}
---
+++ src/Infrastructure/Interfaces/Repositories/IDoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Status = Domain.Enums.EmployeeStatus;
+
+namespace Infrastructure.Interfaces.Repositories;
+
+public interface IDoctorRepository : IBaseRepository<Doctor>
+{
+    Task ChangeStatusAsync(int id, Status status);
+
+    Task<IQueryable<Doctor>> GetByOfficeAsync(int officeId, CancellationToken cancellationToken = default);
+    
+    Task<IQueryable<Doctor>> GetBySpecializationAsync(int specializationId,
+        CancellationToken cancellationToken = default);
+}
