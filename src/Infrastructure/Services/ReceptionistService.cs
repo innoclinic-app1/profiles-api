f2729e800f4faa26c32ffc0707249574316de939
Andrew Shumel <155365407+capybo@users.noreply.github.com>
2024-01-16 10:24:29 +0300

    Refactored receptionist service

--- src/Infrastructure/Services/ReceptionistService.cs
+++ src/Infrastructure/Services/ReceptionistService.cs
@@ -1,6 +1,23 @@
-﻿namespace Infrastructure.Services;
+﻿using Domain.Dtos.Receptionists;
+using Domain.Entities;
+using Infrastructure.Interfaces.Repositories;
+using Infrastructure.Interfaces.Services;
+using Mapster;
 
-public class ReceptionistService
+namespace Infrastructure.Services;
+
+public class ReceptionistService : BaseService<Receptionist, ReceptionistDto, ReceptionistCreateDto, ReceptionistUpdateDto>, IReceptionistService
 {
-    
+    private IReceptionistRepository Repository { get; }
+    public ReceptionistService(IReceptionistRepository repository) : base(repository)
+    {
+        Repository = repository;
+    }
+
+    public async Task<ICollection<ReceptionistDto>> GetManyAsync(string name, int skip, int take, CancellationToken cancellationToken = default)
+    {
+        var receptionists = await Repository.GetManyAsync(name, skip, take, cancellationToken);
+        
+        return receptionists.Adapt<ICollection<ReceptionistDto>>();
+    }
 }
